069ad10bca131f6745ace7509dd33d3471de752d
Mr Den <denyszhyla2000@gmail.com>
2025-05-25 15:56:38 +0300

    Refactor Home component: add search input functionality and format date display

--- v1/src/pages/Home/Home.js
+++ v1/src/pages/Home/Home.js
@@ -1,9 +1,31 @@
 import './Home.css';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import AdditionalOptions from '../../components/AdditionalOptions/AdditionalOptions';
 
 export default function Home() {
+    const today = new Date();
+
+    const options = {
+    weekday: 'short',   // "Tue"
+    day: '2-digit',     // "15"
+    month: 'short'      // "Apr"
+    };
+
+    const formattedDate = today.toLocaleDateString('en-US', options);
+    console.log(formattedDate); // "Tue, 15 Apr"
+
+    const [text, setText] = useState("Find loosen one..."); // Стан для тексту
+
+    const handleChange = (event) => {
+        setText(event.target.value); // Оновлення стану при зміні
+    };
+
+
     return (
         <div class="home">
+            <AdditionalOptions />
+
             <div class="div">
                 <div class="head">
                     <Link to="/additional_options"><img class="user-icon" src="https://c.animaapp.com/maodbhnhX64j70/img/user-icon.png" /></Link>
@@ -11,7 +33,7 @@ export default function Home() {
                 </div>
                 <div class="date">
                     <div class="text-wrapper-2">Today</div>
-                    <div class="text-wrapper-3">Tue, 15 Apr</div>
+                    <div class="text-wrapper-3">{formattedDate}</div>
                     <Link to="/make_a_note"><div class="new">
                         <div class="overlap-group">
                             <img class="line" src="https://c.animaapp.com/maodbhnhX64j70/img/line-7.svg" />
@@ -20,9 +42,20 @@ export default function Home() {
                     </div></Link>
                 </div>
                 <div class="search">
+                    <input 
+                        class="search_input"
+                        type="text"
+                        id="serach"
+                        name="search"
+                        value={text}
+                        onChange={handleChange}
+                    />
+                    <img class="search_input_img" src="https://c.animaapp.com/maodbhnhX64j70/img/search-1.png" />
+                </div>
+                {/* <div class="search">
                     <div class="text-wrapper-4">Find loosen one...</div>
                     <img class="search-2" src="https://c.animaapp.com/maodbhnhX64j70/img/search-1.png" />
-                </div>
+                </div> */}
                 <div class="frame">
                     <img class="arrow-2" src="https://c.animaapp.com/maodbhnhX64j70/img/arrow.png" />
                     <div class="text-wrapper-5">my notes</div>
