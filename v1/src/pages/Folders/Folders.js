e9fe50b8d4e12a7ada67b91c53e5101757c95a14
Mr Den <denyszhyla2000@gmail.com>
2025-05-30 22:44:56 +0300

    Fix image path

--- v1/src/pages/Folders/Folders.js
+++ v1/src/pages/Folders/Folders.js
@@ -28,17 +28,17 @@ export default function Folders() {
                     <div class="div-2">
                         <Link to="/home"><div class="text-wrapper-5">folders</div></Link>
                         <Link to="/open_folders"><div class="studying">
-                            <img class="line" src="/images/Line_r.svg" />
+                            <img class="line" src="/v1/build/images/Line_r.svg" />
                             <div class="text-wrapper-6">studying</div>
                             <img class="arrow" src="https://c.animaapp.com/maodp9luJPrwVp/img/arrow.png" />
                         </div></Link>
                         <div class="studying-2">
-                            <img class="line" src="/images/Line_g.svg" />
+                            <img class="line" src="/v1/build/images/Line_g.svg" />
                             <div class="text-wrapper-6">traveling</div>
                             <img class="arrow" src="https://c.animaapp.com/maodp9luJPrwVp/img/arrow-1.png" />
                         </div>
                         <div class="studying-3">
-                            <img class="line" src="/images/Line_y.svg" />
+                            <img class="line" src="/v1/build/images/Line_y.svg" />
                             <div class="text-wrapper-6">hobby</div>
                             <img class="arrow" src="https://c.animaapp.com/maodp9luJPrwVp/img/arrow-2.png" />
                         </div>
--- v1/src/pages/SignIn/SignIn.js
+++ v1/src/pages/SignIn/SignIn.js
@@ -88,11 +88,11 @@ export default function SignIn() {
                     className="eye"
                     onClick={() => setShowPassword((prev) => !prev)}
                     src={
-                      showPassword
+                      !showPassword
                         ? "https://c.animaapp.com/mb18h967WZb8A8/img/eye-1.png"
-                        : "/images/hidden.png" 
+                        : "/v1/build/images/hidden.png" 
                     }
-                    alt={showPassword ? "Hide password" : "Show password"}
+                    alt={!showPassword ? "Hide password" : "Show password"}
                     style={{ cursor: "pointer" }}
                   />
                 </div>
