2a72ec05625aff685dfd7ef3c4868016bf66c530
davidparhut <150389412+davidparhut@users.noreply.github.com>
2025-06-06 19:50:22 +0300

    Update MakeANote.js

--- v1/src/pages/MakeANote/MakeANote.js
+++ v1/src/pages/MakeANote/MakeANote.js
@@ -1,30 +1,69 @@
 import './MakeANote.css';
-import { useEffect } from "react";
-
-import './MakeANote.css'; 
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { addNoteForCurrentUser } from "./addNoteForCurrentUser"; // імпортуй свою функцію
 
 export default function MakeANote() {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!title.trim() || !content.trim()) {
+            setMessage("Введіть заголовок і текст нотатки");
+            return;
+        }
+        try {
+            await addNoteForCurrentUser(title, content);
+            setMessage("Нотатку збережено!");
+            setTitle("");
+            setContent("");
+        } catch (err) {
+            setMessage("Помилка збереження нотатки");
+        }
+    };
+
     return (
-        <div class="make-a-note">
-            <div class="div">
-                <div class="date">
-                    <div class="text-wrapper">Today</div>
-                    <div class="text-wrapper-2">Tue, 15 Apr</div>
+        <div className="make-a-note">
+            <div className="div">
+                <div className="date">
+                    <div className="text-wrapper">Today</div>
+                    <div className="text-wrapper-2">Tue, 15 Apr</div>
                 </div>
-                <Link to="/home"><img class="arrow" src="https://c.animaapp.com/maoez0i4MW0y0J/img/arrow-1.svg" /></Link>
-                <img class="line" src="https://c.animaapp.com/maoez0i4MW0y0J/img/line-9.svg" />
-                <img class="export" src="https://c.animaapp.com/maoez0i4MW0y0J/img/export-1.png" />
-                <div class="overlap-group">
-                    <div class="view">
-                        <div class="text-wrapper-3">A</div>
-                        <img class="checkbox" src="https://c.animaapp.com/maoez0i4MW0y0J/img/checkbox-1.png" />
-                        <img class="list" src="https://c.animaapp.com/maoez0i4MW0y0J/img/list-1.png" />
-                        <img class="paper-clip" src="https://c.animaapp.com/maoez0i4MW0y0J/img/paper-clip-1.png" />
-                        <img class="pen" src="https://c.animaapp.com/maoez0i4MW0y0J/img/pen-1.png" />
-                        <img class="voice" src="https://c.animaapp.com/maoez0i4MW0y0J/img/voice-1.png" />
+                <Link to="/home">
+                    <img className="arrow" src="https://c.animaapp.com/maoez0i4MW0y0J/img/arrow-1.svg" alt="Back" />
+                </Link>
+                <img className="line" src="https://c.animaapp.com/maoez0i4MW0y0J/img/line-9.svg" alt="Line" />
+                <img className="export" src="https://c.animaapp.com/maoez0i4MW0y0J/img/export-1.png" alt="Export" />
+                <div className="overlap-group">
+                    <div className="view">
+                        <div className="text-wrapper-3">A</div>
+                        <img className="checkbox" src="https://c.animaapp.com/maoez0i4MW0y0J/img/checkbox-1.png" alt="Checkbox" />
+                        <img className="list" src="https://c.animaapp.com/maoez0i4MW0y0J/img/list-1.png" alt="List" />
+                        <img className="paper-clip" src="https://c.animaapp.com/maoez0i4MW0y0J/img/paper-clip-1.png" alt="Paperclip" />
+                        <img className="pen" src="https://c.animaapp.com/maoez0i4MW0y0J/img/pen-1.png" alt="Pen" />
+                        <img className="voice" src="https://c.animaapp.com/maoez0i4MW0y0J/img/voice-1.png" alt="Voice" />
                     </div>
                 </div>
+                <form className="note-form" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Заголовок"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="note-title"
+                    />
+                    <textarea
+                        placeholder="Ваша нотатка..."
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        className="note-content"
+                        rows={5}
+                    />
+                    <button type="submit" className="note-save-btn">Зберегти нотатку</button>
+                    {message && <div className="note-message">{message}</div>}
+                </form>
             </div>
         </div>
     );
